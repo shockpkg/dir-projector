5d7b7d87136f1cfdb29d64356810c72529204fe2
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2020-05-05 19:04:37 -0400

    Removing some unused code

--- src/util.ts
+++ src/util.ts
@@ -3,51 +3,6 @@ import zlib from 'zlib';
 import {launchers} from './launchers';
 
 /**
- * Default value if value is undefined.
- *
- * @param value Value.
- * @param defaultValue Default value.
- * @returns Value or the default value if undefined.
- */
-export function defaultValue<T, U>(
-	value: T,
-	defaultValue: U
-): Exclude<T | U, undefined> {
-	// eslint-disable-next-line no-undefined
-	return value === undefined ? defaultValue : (value as any);
-}
-
-/**
- * Default null if value is undefined.
- *
- * @param value Value.
- * @returns Value or null if undefined.
- */
-export function defaultNull<T>(value: T) {
-	return defaultValue(value, null);
-}
-
-/**
- * Default false if value is undefined.
- *
- * @param value Value.
- * @returns Value or false if undefined.
- */
-export function defaultFalse<T>(value: T) {
-	return defaultValue(value, false);
-}
-
-/**
- * Default true if value is undefined.
- *
- * @param value Value.
- * @returns Value or true if undefined.
- */
-export function defaultTrue<T>(value: T) {
-	return defaultValue(value, true);
-}
-
-/**
  * Create return value once.
  *
  * @param create Create function.
