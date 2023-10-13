66b0d002d4cf7f7c85875245f5d8c616b2e27223
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2023-10-13 02:54:27 -0400

    Reduced uses of Buffer, added read size checking

--- src/bundle/otto/windows.ts
+++ src/bundle/otto/windows.ts
@@ -51,16 +51,25 @@ export class BundleOttoWindows extends BundleOtto {
 	protected async _writeLauncher() {
 		const {path, projector} = this;
 
-		const data = Buffer.alloc(4);
+		const d = new Uint8Array(4);
+		const v = new DataView(d.buffer, d.byteOffset, d.byteLength);
 		const f = await open(projector.path, 'r');
+		let r;
 		try {
-			await f.read(data, 0, 4, 60);
-			await f.read(data, 0, 2, data.readUInt32LE() + 4);
+			r = await f.read(d, 0, 4, 60);
+			if (r.bytesRead < 4) {
+				throw new Error('Unknown format');
+			}
+
+			r = await f.read(d, 0, 2, v.getUint32(0, true) + 4);
+			if (r.bytesRead < 2) {
+				throw new Error('Unknown format');
+			}
 		} finally {
 			await f.close();
 		}
 
-		const machine = data.readUInt16LE();
+		const machine = v.getUint16(0, true);
 		let launcher = null;
 		switch (machine) {
 			case 0x14c: {
